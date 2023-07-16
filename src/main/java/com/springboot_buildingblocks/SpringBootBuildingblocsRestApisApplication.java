9c8b9e0fb508f47ca1d24bbbec996f90a4efebc7
Vishal Singh <vishalsingh.vh99@gmail.com>
2023-07-16 19:40:44 +0530

    First Commit

---
+++ src/main/java/com/springboot_buildingblocks/SpringBootBuildingblocsRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_buildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildingblocsRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuildingblocsRestApisApplication.class, args);
+	}
+
+}
